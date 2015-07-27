8c394689a01d21b8c206f846f7644ec8dd2b0190
gEvent <gevent@CSSI2015-24.local>
2015-07-27 11:58:40 -0400

    edited profile.html and main.js to add "how are you doing" for profile page.

--- Motivationization/main.py
+++ Motivationization/main.py
@@ -19,10 +19,12 @@ import os
 import jinja2
 from google.appengine.ext import ndb
 
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
+    ('/')
 ], debug=True)
---
+++ Motivationization/static/main.js
@@ -0,0 +1,12 @@
+function sad() {
+  alert("I'm sorry, please check out our motivational quotes
+          or some gifs to make you laugh")
+};
+
+function neutral() {
+  alert("You could use a little laugh, check out some gifs")
+};
+
+function happy(){
+  alert("Great, make sure to make someone else smile today")
+};
