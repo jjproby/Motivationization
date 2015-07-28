bd73c755053923039b90fb72f350693a4a4cf594
gEvent <gevent@CSSI2015-7.local>
2015-07-28 17:43:07 -0400

    Users now added to database. And login is required

--- Motivationization/main.py
+++ Motivationization/main.py
@@ -72,12 +72,6 @@ class MainHandler(webapp2.RequestHandler):
             template = jinja_environment.get_template('templates/main.html')
             self.response.out.write(template.render({"user": user.nickname()}))
             self.response.out.write('(<a href="%s">sign out</a>)' % users.create_logout_url('/'))
-        else:
-            greeting = ('<a href="%s">Sign in</a>' %
-                        users.create_login_url('/'))
-
-            self.response.out.write('<html><body>%s</body></html>' % greeting)
-
 
 
 class SallyHandler(webapp2.RequestHandler):
