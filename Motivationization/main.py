be0097e505e353f5fd3ea4b84926c42ef0750f66
gEvent <gevent@CSSI2015-7.local>
2015-07-29 15:27:26 -0400

    Favorites are now stored. Only need a template to show it

--- Motivationization/main.py
+++ Motivationization/main.py
@@ -184,7 +184,7 @@ class MGifHandler(webapp2.RequestHandler):
         template = jinja_environment.get_template('templates/motivation.html')
         self.response.out.write(template.render({'results': gif_url}))
 
-'''class Favorites(webapp2.RequestHandler):
+class Favorites(webapp2.RequestHandler):
     def get(self):
         base_url = 'http://api.giphy.com/v1/gifs/search?q='
         api_key_url = '&api_key=dc6zaTOxFJmzC&limit=40'
